6a4d7e4c8be1681c353e2d3bddf81d4ff6dc9220
Joyce Lee <joycelee901@gmail.com>
2021-05-20 13:04:13 -0500

    commit tree e4ac1ca9f01c79f029b7d0eafba93fe4d9b96e4d

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/*This lab is challenging! You will need to iterate through the tutorials array, modifying the name of each tutorial. To do this, you will also need to access and modify each individual word.
+
+Some questions to consider:
+
+How can we "iterate" through individual words in a string?
+Can we execute an iteration inside an iteration? How?
+How can we capitalize just the first letter in a word? 
+*/ 
+
+
+//declaring titleCased as a function in a const
 const titleCased = () => {
-  return tutorials
-}
+//returning an iteration of tutorials array and identifying the element (each tutorial)
+  return tutorials.map(function(tutorial) {
+  //taking each tutorial and breaking down to individual words, then iterating through each word
+    return tutorial.split(' ').map(function(word) {
+    //taking each word and identifying the first letter, changing it to upper case and then joining them back
+      return word[0].toUpperCase() + word.substr(1)}).join(' ')})
+  }
+
+/*
+//const titleCased = () => {
+  console.log(tutorials.map(tutorial =>
+    tutorial.split(' ').map(word =>
+      word.charAt(0).toUpperCase() + word.join(' '))
+      //return word.join(' ')
+    ))
+//}*/
